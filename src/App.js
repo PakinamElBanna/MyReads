d7aa1c0c109b198f9737b988d51edda8252e0e9c
Pakinam El Banna <pakinam_banna@hotmail.com>
2018-07-08 00:16:16 +0200

    fix: Handle <Search /> memory leak
    when <Search /> ummounts, the api fetch sometimes returns results after the component has been unmounted, which caused unecessary memory leak.
    To solve this, the fetch request has been moved to the parent component App.js, and the results are now being passed to the search component as props.

--- src/App.js
+++ src/App.js
@@ -10,77 +10,92 @@ class BooksApp extends React.Component {
     currentlyReading: [],
     read: [],
     toRead: [],
-    /**
-    * TODO: Instead of using this state variable to keep track of which page
-    * we're on, use the URL in the browser's address bar. This will ensure that
-    * users can use the browser's back and forward buttons to navigate between
-    * pages, as well as provide a good URL they can bookmark and share.
-    */
-    showSearchPage: false
+    booksFound: [],
+    searching: false
   }
 
-  componentDidMount() {
-    BooksAPI.getAll()
-    .then((books) => {
-      const currentlyReading = [];
-      const toRead = [];
-      const read = [];
-      for (let book of books) {
-        var shelf = book.shelf;
-        switch(shelf) {
-          case 'read': {
-            read.push(book);
-            break;
-          }
-          case 'wantToRead': {
-            toRead.push(book);
-            break;
-          }
-          case 'currentlyReading': {
-            currentlyReading.push(book);
-            break;
-          }
-          default: {
-            break;
-          }
+  handleSearchInput = term => {
+    this.setState((oldState) => ({
+      searching: true
+    }))
+    BooksAPI.search(term)
+    .then((booksFound) => {
+      this.setState((oldState) => ({
+        booksFound,
+        searching: false
+      }))
+    }
+  )
+}
+
+resetSearch = () => {
+  this.setState({
+    searching: false,
+    booksFound: []
+  })
+}
+
+componentDidMount() {
+  BooksAPI.getAll()
+  .then((books) => {
+    const currentlyReading = [];
+    const toRead = [];
+    const read = [];
+    for (let book of books) {
+      var shelf = book.shelf;
+      switch(shelf) {
+        case 'read': {
+          read.push(book);
+          break;
+        }
+        case 'wantToRead': {
+          toRead.push(book);
+          break;
+        }
+        case 'currentlyReading': {
+          currentlyReading.push(book);
+          break;
+        }
+        default: {
+          break;
         }
       }
-      this.setState(oldState => ({
-        currentlyReading,
-        toRead,
-        read,
-        books
-      }))
-    })
-  }
+    }
+    this.setState(oldState => ({
+      currentlyReading,
+      toRead,
+      read,
+      books
+    }))
+  })
+}
 
-  render() {
-    console.log(this.state.books)
-    const { currentlyReading, read, toRead } = this.state;
-    return (
-      <div className="app">
-      <Route exact path="/" render={()=>(
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <BookList title={'Currently Reading'} books={currentlyReading}/>
-            <BookList title={'Want to Read'} books={toRead} />
-            <BookList title={'Read'} books={read} />
-          </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
-        </div>
-      )} />
+render() {
+  const { currentlyReading, read, toRead } = this.state;
+  return (
+    <div className="app">
+    <Route exact path="/" render={()=>(
+      <div className="list-books">
+      <div className="list-books-title">
+      <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+      <BookList title={'Currently Reading'} books={currentlyReading}/>
+      <BookList title={'Want to Read'} books={toRead} />
+      <BookList title={'Read'} books={read} />
+      </div>
+      <div className="open-search">
+      <Link to="/search">Add a book</Link>
+      </div>
+      </div>
+    )} />
     <Route path='/search' render={({history}) => (
-      <Search />
+      <Search handleSearchInput={this.handleSearchInput} books={this.state.booksFound} searching={this.state.searching} resetSearch={this.resetSearch}/>
     )}
     />
-      </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
 export default BooksApp
--- src/booklist.js
+++ src/booklist.js
@@ -9,16 +9,16 @@ class BookList extends Component {
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {books.map((book) => (
-                  <li key={book.id}>
-                    <Book title={book.title} authors={book.authors} shelf={book.shelf} cover={book.imageLinks}/>
-                  </li>
-                ))
-            }
-          </ol>
-        </div>
+          {books.map((book) => (
+            <li key={book.id}>
+            <Book title={book.title} authors={book.authors} shelf={book.shelf} cover={book.imageLinks}/>
+            </li>
+          ))
+        }
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+}
 }
 export default BookList;
